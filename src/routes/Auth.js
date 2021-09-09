5ffae87ed552524fd5b847df993163de50afcfb8
hariose <67162120+hariose@users.noreply.github.com>
2021-09-09 20:37:17 +0900

    chapter 2 Commit

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,117 @@
-import React from 'react';
+import {
+  createUserWithEmailAndPassword,
+  signInWithPhoneNumber,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "@firebase/auth";
+import { authService, firebaseInstance } from "fbase";
+import { useState } from "react";
 
-const Auth = () => <span>Auth</span>
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
 
-export default Auth
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        const data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+        console.log(data);
+      } else {
+        const data = await signInWithPhoneNumber(authService, email, password);
+        console.log(data);
+      }
+    } catch (error) {
+      //console.log(error);
+      setError(error.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  /*const onSocialCLick = async (event) => {
+    console.log(event.target.name);
+  }; */
+
+  const onSocialCLick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    try {
+      if (name === "google") {
+        provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(authService, provider);
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+      } else if (name === "github") {
+        provider = new GithubAuthProvider();
+        const result = await signInWithPopup(authService, provider);
+        const credential = GithubAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <input type="submit" value={newAccount ? "Ceate Account" : "Log in"} />
+        <div>{error}</div>
+      </form>
+
+      <span onClick={toggleAccount}>
+        {newAccount ? "sign up" : "Create Account"}
+      </span>
+
+      <div>
+        <button onClick={onSocialCLick} name="google">
+          Google
+        </button>
+        <button onClick={onSocialCLick} name="github">
+          Github
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React from "react";
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>
+const Profile = () => {
+  const history = useHistory();
 
-export default Profile
+  const onLongOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLongOutClick}>Long Out</button>
+    </>
+  );
+};
+
+export default Profile;
