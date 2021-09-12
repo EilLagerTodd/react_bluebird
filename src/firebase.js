e9c4c79949065709710cc1c75ab8c537fbb21add
hariose <67162120+hariose@users.noreply.github.com>
2021-09-12 16:31:47 +0900

    Setting

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaqBQhq8P1rYWcxBVuHA-RRCAJEbMMpns",
+  authDomain: "twittercoloneproject.firebaseapp.com",
+  projectId: "twittercoloneproject",
+  storageBucket: "twittercoloneproject.appspot.com",
+  messagingSenderId: "82177976661",
+  appId: "1:82177976661:web:ad86097b77a752d300e7dc",
+};
+
+export default firebase.initializeApp(firebaseConfig);
