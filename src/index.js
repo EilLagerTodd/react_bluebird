46f7ab64945b87436d48a99909d130507c446e52
hariose <67162120+hariose@users.noreply.github.com>
2021-09-12 20:51:04 +0900

    File Change (Del)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import firebase from "./firebase";
 import App from "./components/App";
 
 console.log(firebase);
->>>>>>> Stashed changes
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
