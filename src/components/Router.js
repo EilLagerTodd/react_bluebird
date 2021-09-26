256cd2f0f748f7e96773e88b453531edb6e57e3a
hariose <67162120+hariose@users.noreply.github.com>
2021-09-27 04:39:15 +0900

    Final

--- src/components/Router.js
+++ src/components/Router.js
@@ -10,14 +10,23 @@ const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
       <div>{isLoggedIn && <Navigation userObj={userObj} />}</div>
       <Switch>
         {isLoggedIn ? (
-          <>
+          <div
+            style={{
+              maxWidth: 890,
+              width: "100%",
+              margin: "0 auto",
+              marginTop: 80,
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
             <Route exact path="/">
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
               <Profile refreshUser={refreshUser} userObj={userObj} />
             </Route>
-          </>
+          </div>
         ) : (
           <Route exact path="/">
             <Auth />
